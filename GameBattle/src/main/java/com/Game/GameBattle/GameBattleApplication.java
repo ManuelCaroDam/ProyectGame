14de3d42c601d90e7f54511db7194f5004499f3c
caro.deman20 <caro.deman20@triana.salesianos.edu>
2022-03-04 12:54:16 +0100

    "Re inicio"

---
+++ GameBattle/src/main/java/com/Game/GameBattle/GameBattleApplication.java
@@ -0,0 +1,13 @@
+package com.Game.GameBattle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameBattleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameBattleApplication.class, args);
+	}
+
+}
